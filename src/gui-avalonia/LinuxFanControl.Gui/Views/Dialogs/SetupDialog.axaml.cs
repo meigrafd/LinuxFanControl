080bf12a4b83a899c694940f9a2baca53dc9a40b
meigrafd <krunzer@gmail.com>
2025-09-14 01:00:37 +0200

    fixed dashboard

--- src/gui-avalonia/LinuxFanControl.Gui/Views/Dialogs/SetupDialog.axaml.cs
+++ src/gui-avalonia/LinuxFanControl.Gui/Views/Dialogs/SetupDialog.axaml.cs
@@ -1,40 +1,19 @@
 using Avalonia.Controls;
-using LinuxFanControl.Gui.ViewModels.Dialogs;
+using Avalonia.Markup.Xaml;
 
 namespace LinuxFanControl.Gui.Views.Dialogs
 {
     public partial class SetupDialog : Window
     {
-        public bool ShouldRunDetection { get; private set; }
-        public bool ShouldRunCalibration { get; private set; }
-
         public SetupDialog()
         {
             InitializeComponent();
+        }
 
-            var vm = new SetupDialogViewModel();
-            DataContext = vm;
-
-            var btnApply  = this.FindControl<Button>("BtnApply");
-            var btnCancel = this.FindControl<Button>("BtnCancel");
-
-            if (btnApply is not null)
-            {
-                btnApply.Click += (_, __) =>
-                {
-                    if (DataContext is SetupDialogViewModel m)
-                    {
-                        ShouldRunDetection  = m.RunDetection;
-                        ShouldRunCalibration = m.RunCalibration;
-                    }
-                    Close(true);
-                };
-            }
-
-            if (btnCancel is not null)
-            {
-                btnCancel.Click += (_, __) => Close(false);
-            }
+        // Explicit XAML load to avoid generator issues
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
         }
     }
 }
