867fd68087cc7a4fb7eec49c6d26709c485a2662
meigrafd <krunzer@gmail.com>
2025-09-13 10:58:16 +0200

    switched gui to dotnet9 + avalonia

--- src/gui-avalonia/LinuxFanControl.Gui/obj/Debug/net9.0/linux-x64/LinuxFanControl.Gui.AssemblyInfo.cs
+++ src/gui-avalonia/LinuxFanControl.Gui/obj/Debug/net9.0/linux-x64/LinuxFanControl.Gui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinuxFanControl.Gui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee36c2d91cb194168c986c68574cc24ae5ec479a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68a6b6e50ec2a764a55e73ba19856e457ec978a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinuxFanControl.Gui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinuxFanControl.Gui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
