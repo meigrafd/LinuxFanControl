ccd8da62403b9ce3c55d91236732023d2151c5da
meigrafd <krunzer@gmail.com>
2025-09-14 03:33:56 +0200

    fixed dashboard

--- src/gui-avalonia/LinuxFanControl.Gui/Views/Controls/FanLogo.axaml.cs
+++ src/gui-avalonia/LinuxFanControl.Gui/Views/Controls/FanLogo.axaml.cs
@@ -1,41 +1,14 @@
 // (c) 2025 LinuxFanControl contributors. MIT License.
 #nullable enable
 using Avalonia.Controls;
-using Avalonia.Media;
-using Avalonia.Controls.Shapes;
 
 namespace LinuxFanControl.Gui.Views.Controls
 {
-    public partial class FanLogo : UserControl
+    public partial class FanTile : UserControl
     {
-        public FanLogo()
+        public FanTile()
         {
-            var canvas = new Canvas
-            {
-                Width = 64,
-                Height = 64
-            };
-
-            var accentBrush = this.FindResource("Lfc.Accent") as IBrush ?? Brushes.Blue;
-            var strokeBrush = this.FindResource("Lfc.WindowBg") as IBrush ?? Brushes.Black;
-
-            var ellipse = new Ellipse
-            {
-                Width = 64,
-                Height = 64,
-                Fill = accentBrush
-            };
-
-            var path = new Path
-            {
-                Stroke = strokeBrush,
-                StrokeThickness = 4,
-                Data = Geometry.Parse("M32,0 L32,64 M0,32 L64,32")
-            };
-
-            canvas.Children.Add(ellipse);
-            canvas.Children.Add(path);
-            Content = canvas;
+            InitializeComponent();
         }
     }
 }
