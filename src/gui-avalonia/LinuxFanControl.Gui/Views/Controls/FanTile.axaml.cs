9044b8e84e5ed3c158ecd579c311711c5ccb6f02
meigrafd <krunzer@gmail.com>
2025-09-14 02:09:21 +0200

    fixed dashboard

--- src/gui-avalonia/LinuxFanControl.Gui/Views/Controls/FanTile.axaml.cs
+++ src/gui-avalonia/LinuxFanControl.Gui/Views/Controls/FanTile.axaml.cs
@@ -3,9 +3,19 @@ using Avalonia.Markup.Xaml;
 
 namespace LinuxFanControl.Gui.Views.Controls
 {
+    /// <summary>
+    /// Dashboard fan tile. Binds to FanTileViewModel (Name, SensorDisplay, RpmDisplay, Duty).
+    /// </summary>
     public partial class FanTile : UserControl
     {
-        public FanTile() { InitializeComponent(); }
-        private void InitializeComponent() => AvaloniaXamlLoader.Load(this);
+        public FanTile()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
     }
 }
